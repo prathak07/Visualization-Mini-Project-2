5cb7d60a808f9dc076d874a92c755a034c8d0737
Prathak Rastogi <prathak07@gmail.com>
2017-04-02 23:48:42 -0400

    Files Added

---
+++ src/GenVisData.py
@@ -0,0 +1,212 @@
+import sys
+import numpy
+import scipy.stats
+import pandas
+import random
+from sklearn import cluster, metrics
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+
+
+directory = '../data/processed/'
+
+
+def random_sampling(data_frame,fraction):
+    print "Doing Random Sampling with fraction "+str(fraction)
+    rows = random.sample(data_frame.index,(int)(len(data_frame)*fraction))
+    # print rows
+    # print data_frame.iloc[rows]
+    return data_frame.iloc[rows]
+
+
+def find_k_for_kmean(data_frame):
+    print "Getting elbow for k-mean clustering by clustering the data with various different k (1-10)"
+    i=1
+    dic = []
+    while(i<=10):
+        km = cluster.KMeans(n_clusters=i)
+        km.fit(data_frame)
+        dic.append((i,km.inertia_))
+        i+=1
+    file_name = directory+'kmean.csv'
+    print "Creating file "+file_name
+    dicDF = pandas.DataFrame(dic)
+    dicDF.columns = ['cluster_size','value']
+    dicDF.to_csv(file_name,sep=',',index=False)
+    print "K by elbow method is "+str(3)
+
+
+
+def stratified_sampling(data_frame,no_of_clusters,fraction):
+    print "Doing stratified sampling where no. of clusters "+str(no_of_clusters)+" and fraction "+str(fraction)
+    kmean = cluster.KMeans(n_clusters=no_of_clusters)
+    kmean.fit(data_frame)
+    data_frame['kmean'] = kmean.labels_
+    # from each cluster get some values
+    result_data_frame = pandas.DataFrame([],columns=data_frame.columns.values)
+    for i in range(no_of_clusters):
+        temp = data_frame.loc[data_frame['kmean']==i]
+        length = len(temp.index)
+        random_index = random.sample(range(length),int(length*fraction))
+        random_data_frame = temp.iloc[random_index]
+        result_data_frame = result_data_frame.append(random_data_frame)
+    result_data_frame = result_data_frame.drop('kmean',1)
+    return result_data_frame
+
+
+def data_pca(df1,df2):
+    print "Starting PCA:"
+    pca = PCA()
+    col_names = df1.columns.values
+    print "Random Sampling"
+    pca.fit_transform(df1)
+    eigen_values_1 = pca.explained_variance_
+    top_1 = pca.components_
+    file_name = directory+'pca_components_random.csv'
+    print "Creating file "+file_name
+    topDF = pandas.DataFrame(top_1)
+    topDF.to_csv(file_name,sep=',',index=False)
+    print "Stratified Sampling"
+    pca.fit_transform(df2)
+    eigen_values_2 = pca.explained_variance_
+    top_2 = pca.components_
+    file_name = directory+'pca_components_stratified.csv'
+    print "Creating file "+file_name
+    topDF = pandas.DataFrame(top_2)
+    topDF.to_csv(file_name,sep=',',index=False)
+    eigen_tuple_list = []
+    for i in range(len(eigen_values_1)):
+        eigen_tuple_list.append((i+1,eigen_values_1[i],eigen_values_2[i]))
+    file_name = directory+'pca_eigens.csv'
+    print "Creating file "+file_name
+    tupDF = pandas.DataFrame(eigen_tuple_list)
+    tupDF.columns = ['variable','col1','col2']
+    tupDF.to_csv(file_name,sep=',',index=False)
+    # By observing the Eigen value plot no_of_components is found to be 8
+    print "Intrinsic Dimensionality: "+str(8)
+    sq_sum_1 = {}
+    for i in range(len(top_1[0])):
+        sum = 0
+        for j in range(8):
+            sum += top_1[j][i]**2
+        s = col_names[i]
+        sq_sum_1[s] = sum
+    sq_sum_2 = {}
+    for i in range(len(top_2[0])):
+        sum = 0
+        for j in range(8):
+            sum += top_2[j][i]**2
+        s = col_names[i]
+        sq_sum_2[s] = sum
+    tup_list = []
+    for i in range(len(col_names)):
+        tup_list.append((col_names[i],sq_sum_1[col_names[i]],sq_sum_2[col_names[i]]))
+    tup_list = sorted(tup_list, key=lambda x:-1*x[2])
+    print "Top 3 PCA Loadings: "+tup_list[0][0]+", "+tup_list[1][0]+' and '+tup_list[2][0]
+    file_name = directory+'scree_loadings.csv'
+    print "Creating file "+file_name
+    tupDF = pandas.DataFrame(tup_list)
+    tupDF.columns = ['variable','col1','col2']
+    tupDF.to_csv(file_name,sep=',',index=False)
+    return tup_list,eigen_values_1,eigen_values_2
+
+
+def find_pca2(df1,df2,file_name):
+    print "PCA 2 started"
+    pca = PCA(n_components=2)
+    random_sample = pandas.DataFrame(pca.fit_transform(df1))
+    stratified_sample = pandas.DataFrame(pca.fit_transform(df2))
+    random_sample.columns = ['PC1','PC2']
+    stratified_sample.columns = ['PC1','PC2']
+    type1 = pandas.DataFrame(numpy.ones([len(random_sample.index),1], dtype=int),columns=['type'])
+    random_sample = pandas.concat([random_sample, type1], axis=1)
+    type2 = pandas.DataFrame(numpy.ones([len(stratified_sample.index),1], dtype=int)+1,columns=['type'])
+    stratified_sample = pandas.concat([stratified_sample, type2], axis=1)
+    sample = pandas.concat([random_sample, stratified_sample], axis=0)
+    print "PCA 2 finished"
+    file_name = directory + file_name
+    print "Creating file "+file_name
+    sample.to_csv(file_name,sep=',',index=False)
+
+
+def find_MDS_euclidean(df1,df2,file_name):
+    print "MDS (euclidean) started"
+    dis_mat_random = metrics.pairwise_distances(df1, metric = 'euclidean')
+    mds = MDS(n_components=2, dissimilarity='precomputed')
+    random_sample = pandas.DataFrame(mds.fit_transform(dis_mat_random))
+    dis_mat_stratified = metrics.pairwise_distances(df2, metric = 'euclidean')
+    mds = MDS(n_components=2, dissimilarity='precomputed')
+    stratified_sample = pandas.DataFrame(mds.fit_transform(dis_mat_stratified))
+    random_sample.columns = ['PC1','PC2']
+    stratified_sample.columns = ['PC1','PC2']
+    type1 = pandas.DataFrame(numpy.ones([len(random_sample.index),1], dtype=int),columns=['type'])
+    random_sample = pandas.concat([random_sample, type1], axis=1)
+    type2 = pandas.DataFrame(numpy.ones([len(stratified_sample.index),1], dtype=int)+1,columns=['type'])
+    stratified_sample = pandas.concat([stratified_sample, type2], axis=1)
+    sample = pandas.concat([random_sample, stratified_sample], axis=0)
+    print "MDS (euclidean) finished"
+    file_name = directory + file_name
+    print "Creating file "+file_name
+    sample.to_csv(file_name,sep=',',index=False)
+
+
+def find_MDS_correlation(df1,df2,file_name):
+    print "MDS (correlation) started"
+    dis_mat_random = metrics.pairwise_distances(df1, metric = 'correlation')
+    mds = MDS(n_components=2, dissimilarity='precomputed')
+    random_sample = pandas.DataFrame(mds.fit_transform(dis_mat_random))
+    dis_mat_stratified = metrics.pairwise_distances(df2, metric = 'correlation')
+    mds = MDS(n_components=2, dissimilarity='precomputed')
+    stratified_sample = pandas.DataFrame(mds.fit_transform(dis_mat_stratified))
+    random_sample.columns = ['PC1','PC2']
+    stratified_sample.columns = ['PC1','PC2']
+    type1 = pandas.DataFrame(numpy.ones([len(random_sample.index),1], dtype=int),columns=['type'])
+    random_sample = pandas.concat([random_sample, type1], axis=1)
+    type2 = pandas.DataFrame(numpy.ones([len(stratified_sample.index),1], dtype=int)+1,columns=['type'])
+    stratified_sample = pandas.concat([stratified_sample, type2], axis=1)
+    sample = pandas.concat([random_sample, stratified_sample], axis=0)
+    print "MDS (correlation) finished"
+    file_name = directory + file_name
+    print "Creating file "+file_name
+    sample.to_csv(file_name,sep=',',index=False)
+
+
+def find_pca3(df1,df2,file_name):
+    print "PCA 3 started"
+    pca = PCA(n_components=3)
+    random_sample = pandas.DataFrame(pca.fit_transform(df1))
+    stratified_sample = pandas.DataFrame(pca.fit_transform(df2))
+    random_sample.columns = ['PC1','PC2','PC3']
+    stratified_sample.columns = ['PC1','PC2','PC3']
+    type1 = pandas.DataFrame(numpy.ones([len(random_sample.index),1], dtype=int),columns=['type'])
+    random_sample = pandas.concat([random_sample, type1], axis=1)
+    type2 = pandas.DataFrame(numpy.ones([len(stratified_sample.index),1], dtype=int)+1,columns=['type'])
+    stratified_sample = pandas.concat([stratified_sample, type2], axis=1)
+    sample = pandas.concat([random_sample, stratified_sample], axis=0)
+    print "PCA 3 finished"
+    file_name = directory + file_name
+    print "Creating file "+file_name
+    sample.to_csv(file_name,sep=',',index=False)
+
+
+def main(filename):
+    data_frame = pandas.read_csv("../data/"+filename)
+    data_frame = data_frame.drop(data_frame.columns[0],axis=1)
+    data_frame = data_frame.fillna(value=0)
+    random_sample = random_sampling(data_frame,0.05)
+    find_k_for_kmean(data_frame)
+    # found the elbow at 3 thus, number of clusters to be created are 3
+    stratified_sample = stratified_sampling(data_frame,3,0.05)
+    data_pca(random_sample,stratified_sample)
+    find_pca2(random_sample,stratified_sample,'pca2.csv')
+    find_MDS_euclidean(random_sample,stratified_sample,'mds_euclidean.csv')
+    find_MDS_correlation(random_sample,stratified_sample,'mds_correlation.csv')
+    find_pca3(random_sample,stratified_sample,'pca3.csv')
+
+
+if __name__=='__main__':
+    if(len(sys.argv)<2):
+        print "Command format: python GenVisData.py <csv_filename>"
+    else:
+        main(sys.argv[1])
---
+++ src/graphs.js
@@ -0,0 +1,467 @@
+function scatter_plot(file_name) {
+    document.getElementById("pca3").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    file_name = "./data/processed/"+file_name;
+    color = ["Green","Red"];
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 100},
+        width = 1020 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var xScale = d3.scale.linear().range([0, width]);
+    var yScale = d3.scale.linear().range([height, 0]);
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+    d3.csv(file_name, function(data) {
+      data.forEach(function(d) {
+                d.PC1 = Number(d.PC1);
+                d.PC2 = Number(d.PC2);
+                d.type = Number(d.type);
+            });
+
+      xScale.domain([d3.min(data, function(d){return d.PC1;})-1, d3.max(data, function(d){return d.PC1;})+1]);
+      yScale.domain([d3.min(data, function(d){return d.PC2;})-1, d3.max(data, function(d){return d.PC2;})+1]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+          .append("text")
+          .attr("class", "label")
+          .attr("x", width)
+          .attr("y", -6)
+          .style("text-anchor", "end")
+          .text("Component 1");
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .append("text")
+          .attr("class", "label")
+          .attr("x",1)
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "start")
+          .text("Component 2");
+
+      svg.selectAll(".dot")
+          .data(data)
+          .enter().append("circle")
+          .attr("class", "dot")
+          .attr("r", 3)
+          .attr("cx", function(d){
+                return xScale(d.PC1);
+            })
+          .attr("cy", function(d){
+                return yScale(d.PC2);
+            })
+          .style("fill", function(d) { return color[d.type-1];})
+          .on("mouseover", function(d) {
+              tooltip.transition()
+                   .duration(200)
+                   .style("opacity", .9);
+              tooltip.html("("+ d.col1.toPrecision(4) + ", " + d.col2.toPrecision(4) + ")")
+                   .style("font-weight","bold")
+                   .style("left", (d3.event.pageX + 5) + "px")
+                   .style("top", (d3.event.pageY - 28) + "px");
+          })
+          .on("mouseout", function(d) {
+              tooltip.transition()
+                   .duration(200)
+                   .style("opacity", 0);
+          });
+
+      svg.append("text")
+          .attr("transform", "translate(" + (width-200) + "," + 10 + ")")
+          .attr("dy", ".35em")
+          .attr("text-anchor", "start")
+          .style("fill", "Green")
+          .text("Random");
+
+      svg.append("text")
+          .attr("transform", "translate(" + (width-100) + "," + 10 + ")")
+          .attr("dy", ".35em")
+          .attr("text-anchor", "start")
+          .style("fill", "Red")
+          .text("Stratified");
+    });
+}
+
+function pca3() {
+    document.getElementById("pca3").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    var file_name = "./data/processed/pca3.csv";
+    var color = ['Green','Red']
+
+    var width = 650,
+        size = 200,
+        padding = 25;
+
+    var x = d3.scale.linear()
+        .range([padding / 2, size - padding / 2]);
+
+    var y = d3.scale.linear()
+        .range([size - padding / 2, padding / 2]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(6);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(6);
+
+    d3.csv(file_name, function(error, data) {
+      if (error) throw error;
+
+      var domainByTrait = {},
+          traits = d3.keys(data[0]).filter(function(d) { return d !== "type"; }),
+          n = traits.length;
+
+      traits.forEach(function(trait) {
+        domainByTrait[trait] = d3.extent(data, function(d) { return Number(d[trait]); });
+      });
+
+      xAxis.tickSize(size * n);
+      yAxis.tickSize(-size * n);
+
+      var svg = d3.select("#pca3").append("svg")
+          .attr("width", size * n + padding)
+          .attr("height", size * n + padding)
+          .append("g")
+          .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+
+      svg.selectAll(".x.axis")
+          .data(traits)
+          .enter().append("g")
+          .attr("class", "x axis")
+          .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
+          .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+
+      svg.selectAll(".y.axis")
+          .data(traits)
+          .enter().append("g")
+          .attr("class", "y axis")
+          .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
+          .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+
+      var cell = svg.selectAll(".cell")
+          .data(cross(traits, traits))
+          .enter().append("g")
+          .attr("class", "cell")
+          .attr("transform", function(d) {
+            // console.log(d);
+            return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")";
+          })
+          .each(plot);
+
+      cell.filter(function(d) { return d.i === d.j; }).append("text")
+          .attr("x", padding)
+          .attr("y", padding)
+          .attr("dy", ".71em")
+          .text(function(d) { return d.x; });
+
+      function plot(p) {
+        var cell = d3.select(this);
+
+        x.domain(domainByTrait[p.x]);
+        y.domain(domainByTrait[p.y]);
+
+        cell.append("rect")
+            .attr("class", "frame")
+            .attr("x", padding / 2)
+            .attr("y", padding / 2)
+            .attr("width", size - padding)
+            .attr("height", size - padding);
+
+        cell.selectAll("circle")
+            .data(data)
+            .enter().append("circle")
+            .attr("cx", function(d) { return x(d[p.x]);})
+            .attr("cy", function(d) { return y(d[p.y]); })
+            .attr("r", 3)
+            .style("fill", function(d) {
+              // console.log(d);
+              return color[Number(d.type)-1];
+            });
+      }
+    });
+
+    function cross(a, b) {
+      var c = [], n = a.length, m = b.length, i, j;
+      for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+      return c;
+    }
+}
+
+function eigen_plot() {
+    document.getElementById("pca3").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    file_name = "./data/processed/pca_eigens.csv";
+    color = ["Green","Red"];
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 100},
+        width = 1020 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(36);
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+    var colLine1 = d3.svg.line()
+        .x(function(d) { return x(d.variable); })
+        .y(function(d) { return y(d.col1); });
+
+    var colLine2 = d3.svg.line()
+        .x(function(d) { return x(d.variable); })
+        .y(function(d) { return y(d.col2); });
+
+    d3.csv(file_name, function(error, data) {
+        data.forEach(function(d) {
+            d.variable = +d.variable;
+            d.col1 = +d.col1;
+            d.col2 = +d.col2;
+        });
+
+        x.domain([0, d3.max(data, function(d) { return d.variable; })]);
+        y.domain([0, d3.max(data, function(d) { return Math.max(d.col1, d.col2); })]);
+
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", colLine1(data));
+
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", "Red")
+            .attr("d", colLine2(data));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("PCA Components");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -75)
+            .attr("x",0 - (height / 2))
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Eigen Value");
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width-200) + "," + 10 + ")")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "start")
+            .style("fill", "Green")
+            .text("Random");
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width-100) + "," + 10 + ")")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "start")
+            .style("fill", "Red")
+            .text("Stratified");
+
+    });
+}
+
+function loading_plot() {
+    document.getElementById("pca3").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    file_name = "./data/processed/scree_loadings.csv";
+    color = ["Green","Red"];
+
+    var margin = {top: 20, right: 20, bottom: 150, left: 100},
+        width = 1020 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.ordinal().rangeRoundPoints([0 , width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom");
+    var yAxis = d3.svg.axis().scale(y).orient("left");
+
+    var colLine1 = d3.svg.line()
+        .x(function(d) { return x(d.variable); })
+        .y(function(d) { return y(d.col1); })
+        .interpolate("linear");
+
+    var colLine2 = d3.svg.line()
+        .x(function(d) { return x(d.variable); })
+        .y(function(d) { return y(d.col2); })
+        .interpolate("linear");
+
+    d3.csv(file_name, function(error, data) {
+        data.forEach(function(d) {
+            d.variable = d.variable;
+            d.col1 = +d.col1;
+            d.col2 = +d.col2;
+        });
+        console.log(data);
+
+        x.domain(data.map(function(d) { return d.variable; }));
+        y.domain([0, d3.max(data, function(d) { return Math.max(d.col1, d.col2); })]);
+
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", colLine1(data));
+
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", "Red")
+            .attr("d", colLine2(data));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)")
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -75)
+            .attr("x",0 - (height / 2))
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Loadings");
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width-200) + "," + 10 + ")")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "start")
+            .style("fill", "Green")
+            .text("Random");
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width-100) + "," + 10 + ")")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "start")
+            .style("fill", "Red")
+            .text("Stratified");
+
+    });
+}
+
+function kmean_plot() {
+    document.getElementById("pca3").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    file_name = "./data/processed/kmean.csv";
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 100},
+        width = 1020 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10);
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+    var colLine = d3.svg.line()
+        .x(function(d) { return x(d.cluster_size); })
+        .y(function(d) { return y(d.value); });
+
+    d3.csv(file_name, function(error, data) {
+        data.forEach(function(d) {
+            d.cluster_size = +d.cluster_size;
+            d.value = +d.value;
+        });
+
+        x.domain([0, d3.max(data, function(d) { return d.cluster_size; })]);
+        y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", "steelblue")
+            .attr("d", colLine(data));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Cluster Size");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("x",1)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Squared Sum Error");
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width-200) + "," + 10 + ")")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "start")
+            .style("fill", "steelblue")
+            .text("Elbow Plot");
+
+    });
+}
